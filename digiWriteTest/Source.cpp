c31c414cf1c899b50e0f90c59846a596bd99cb23
Tomas87 <tomas.krajcoviech@gmail.com>
2017-07-27 22:49:11 +0200

    output int dist

--- digiWriteTest/Source.cpp
+++ digiWriteTest/Source.cpp
@@ -5,15 +5,19 @@ using namespace std;
 #define HIGH 1
 #define LOW 0
 
-uint8_t _pin[2] = { 8,9 };
+const uint8_t hi = 1;
+const uint8_t lo = 0;
+uint8_t te = 254;
 
-void outTest(int mask) {
-	uint8_t numpins = 2; // DRIVER
+uint8_t _pin[3] = { 7,8,9 };
+
+void outTest(uint8_t mask) {
+	uint8_t numpins = 3; // DRIVER
 	uint8_t i;
 	uint8_t tab = 0b1001;
 	for (i = 0; i < numpins; i++)
 	{
-		cout << "PIN No: " << int(_pin[i]) << "    Output: " << (mask & (1 << i)) ? HIGH : LOW; 
+		cout << "PIN No: " << int(_pin[i]) << "    Output: " << int((mask & (1 << i)) ? HIGH  : LOW );
 		cout << tab<< tab;
 	}
 	cout << endl; 
@@ -30,9 +34,16 @@ int main() {
 		cout << bitset<8>(a) << endl;
 	}
 	*/
-
-	outTest(0b00);
-	outTest(0b01);
-	outTest(0b00);
+	cout << bitset<16>(te) << endl;
+	outTest(0b100);
+	outTest(0b101);
+	outTest(0b100);
+	outTest(0b110);
+	outTest(0b111);
+	outTest(0b010);
+	outTest(0b011);
+	outTest(0b000);
+	outTest(0b100);
+	outTest(0b101);
 	return 0; 
 }
